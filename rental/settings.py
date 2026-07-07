128a4c29c7f905da53775e238698a4c254738611
varsha <varsha@itegoss.in>
2026-07-07 16:59:09 +0530

    img

--- rental/settings.py
+++ rental/settings.py
@@ -79,16 +79,26 @@ TEMPLATES = [
 
 ENV = os.getenv("ENV", "DEV")
 
-DATABASES = {
+import sys
+if 'test' in sys.argv:
+    DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-}
-}
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': os.getenv('DB_NAME'),
+                'USER': os.getenv('DB_USER'),
+                'PASSWORD': os.getenv('DB_PASSWORD'),
+                'HOST': os.getenv('DB_HOST'),
+                'PORT': os.getenv('DB_PORT'),
+    }
+    }
 
 
 
