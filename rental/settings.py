63504bfcb5a5e72761f8e4c46371af4dbeccb89c
varsha <varsha@itegoss.in>
2026-08-06 16:10:49 +0530

    card size change

--- rental/settings.py
+++ rental/settings.py
@@ -96,6 +96,7 @@ else:
      }
 
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
