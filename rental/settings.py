3287fb4705eb0f10701dd29e7f458cec0e7b0b80
varsha <varsha@itegoss.in>
2026-08-27 14:39:21 +0530

    want to pulsssl

--- rental/settings.py
+++ rental/settings.py
@@ -87,16 +87,17 @@ if 'test' in sys.argv:
     }
 
 else:
-    DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'kys',
-         'USER': 'postgres',
-         'PASSWORD': 'admin',
-         'HOST': 'localhost',
-         'PORT': '5432',
+     DATABASES = {
+             'default': {
+                 'ENGINE': 'django.db.backends.postgresql',
+                 'NAME': os.getenv('DB_NAME'),
+                 'USER': os.getenv('DB_USER'),
+                 'PASSWORD': os.getenv('DB_PASSWORD'),
+                 'HOST': os.getenv('DB_HOST'),
+                 'PORT': os.getenv('DB_PORT'),
      }
- }
+     }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
