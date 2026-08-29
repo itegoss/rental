13e616fa4b3a0e5b6fdff299bbcac63c7e8e9803
varsha <varsha@itegoss.in>
2026-08-29 17:28:02 +0530

    allowed hosst commit

--- rental/settings.py
+++ rental/settings.py
@@ -88,15 +88,15 @@ if 'test' in sys.argv:
 
 else:
     DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'kys',
-         'USER': 'postgres',
-         'PASSWORD': 'admin',
-         'HOST': 'localhost',
-         'PORT': '5432',
+             'default': {
+                 'ENGINE': 'django.db.backends.postgresql',
+                 'NAME': os.getenv('DB_NAME'),
+                 'USER': os.getenv('DB_USER'),
+                 'PASSWORD': os.getenv('DB_PASSWORD'),
+                 'HOST': os.getenv('DB_HOST'),
+                 'PORT': os.getenv('DB_PORT'),
      }
- }
+     }
 
 
 AUTH_PASSWORD_VALIDATORS = [
